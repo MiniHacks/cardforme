89d7a1ba100f29a092e44087bc4f7e3c4b1eb6b9
Ansh Patel <pate1011@umn.edu>
2023-09-23 19:36:50 -0500

    😍 Carousel + Fonts + ViewCard

--- packages/frontend/components/ViewCard.tsx
+++ packages/frontend/components/ViewCard.tsx
@@ -1,25 +1,74 @@
 import React from 'react';
-import { Box, Button, Divider, Heading, Stack, Text, Image } from '@chakra-ui/react';
+import { Box, Button, Heading, Stack, Text, Image, HStack, Flex, Center } from "@chakra-ui/react";
 
 const ViewCard = () => (
-  <Box backgroundColor="#FFFFFF" maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' minW='sm' maxH='lg' minH='450px'>
-    <Image
-      src='images/amex_gold.png'
-      borderRadius='lg'
-    />
-    <Stack mt='6' spacing='3' p='4'>
-      <Heading size='md'>Living room Sofa</Heading>
+  <Box
+    style={{
+      backgroundColor: 'rgba(255, 255, 255, 0.8)', // Adjust opacity as needed
+      backdropFilter: 'blur(10px)', // Adjust the blur value as needed
+      boxShadow: '0px 0px 10px rgba(255, 105, 180, 0.5)' // Pink drop shadow glow
+    }}
+    maxW='sm'
+    borderWidth='1px'
+    borderRadius='lg'
+    overflow='hidden'
+    minW='sm'
+    maxH='lg'
+    minH='450px'
+  >
+    <Center>
+      <Box paddingTop="20px">
+        <Image
+          src='images/amex_platinum.png'
+          borderRadius='lg'
+        />
+      </Box>
+    </Center>
+    <Stack mt='6' spacing='3' p='4' marginLeft='20px' marginRight='20px'>
+      <HStack>
+        <Text
+          bgGradient='linear(to-t, #5200FF, #FF8A00)'
+          bgClip='text'
+          fontFamily="Outfit"
+          fontWeight="600"
+          fontSize="30px"
+        >
+          #1
+        </Text>
+        <Text
+          fontFamily="Outfit"
+          fontWeight="600"
+          fontSize="30px"
+          textColor="#000000"
+        >
+          Amex Platinum
+        </Text>
+      </HStack>
       <Text>
         Cool Text
       </Text>
-      <Text color='blue.600' fontSize='2xl'>
-        $450
-      </Text>
     </Stack>
-    <Divider />
-      <Button variant='solid' colorScheme='blue'>
-        Buy now
-      </Button>
+    <Flex
+      justify='center' // Center horizontally
+      align='center'  // Center vertically
+      direction='column' // Stack children vertically
+      height="50px" // Set the height of the container
+      marginTop="20px"
+    >
+      <Box
+        as='button'
+        color='white'
+        fontWeight='bold'
+        borderRadius='12px'
+        width="85%"
+        height="50px"
+        bgGradient='linear(to-l, #5200FF,#FF0080, #FF8A00)'
+      >
+        <Text >
+          Start Now
+        </Text>
+      </Box>
+    </Flex>
   </Box>
 );
 
--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -8,6 +8,11 @@ export default function Document(): JSX.Element {
         <Head>
             <title>Connect a bank</title>
             <script src="https://cdn.plaid.com/link/v2/stable/link-initialize.js"></script>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+              <link
+                href="https://fonts.googleapis.com/css2?family=Monoton&family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                rel="stylesheet"/>
         </Head>
         <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
