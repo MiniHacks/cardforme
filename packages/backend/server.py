14375f83f612fca329eace8357490926b36b4563
brandon-nguyen-lam <brandonnguyenlam@gmail.com>
2023-09-24 10:14:10 -0500

    😁 sammy oak tree the goat

--- packages/backend/server.py
+++ packages/backend/server.py
@@ -9,7 +9,6 @@ from flask_cors import CORS, cross_origin
 from flask_talisman import Talisman
 import card_backtesting
 import transactions
-import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -21,15 +20,14 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def hello_world():
     return 'Hello World!'
 
-@app.route('/transactions', methods=['GET'])
+@app.route('/transactions', methods=['GET', 'POST'])
 @cross_origin()
 def get_transactions():
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    response = request.get_json()
     try:
-        res = get_transactions().json()
-        current_transactions = Transactions()
+        current_transactions = transactions.Transactions()
 
-        for transaction in res:
+        for transaction in response:
 
             category = transaction['category']
             if len(category) > 1:
@@ -40,48 +38,48 @@ def get_transactions():
             current_transactions.add_transaction(category, transaction['amount'])
 
         res = []
-        res.append((amex_platinum(current_transactions), "amex_platinum"))
-        res.append((amex_gold(current_transactions), "amex_gold"))
-        res.append((amex_blue_cash_everyday(current_transactions), "amex_blue_cash_everyday"))
-        res.append((amex_blue_cash_preferred(current_transactions), "amex_blue_cash_preferred"))
-        res.append((amex_business_gold(current_transactions), "amex_business_gold"))
-        res.append((bofa_travel_rewards(current_transactions), "bofa_travel_rewards"))
-        res.append((bofa_premium_rewards(current_transactions), "bofa_premium_rewards"))
-        res.append((bofa_unlimited_rewards(current_transactions), "bofa_unlimited_rewards"))
-        res.append((barclays_aviator_red_world_elite_mastercard(current_transactions), "barclays_aviator_red_world_elite_mastercard"))
-        res.append((barclays_the_hawaiian_airlines_world_elite_mastercard(current_transactions), "barclays_the_hawaiian_airlines_world_elite_mastercard"))
-        res.append((barclays_jetblue(current_transactions), "barclays_jetblue"))
-        res.append((capital_one_quicksilver_cash_rewards(current_transactions), "capital_one_quicksilver_cash_rewards"))
-        res.append((capital_one_venture_rewards(current_transactions), "capital_one_venture_rewards"))
-        res.append((capital_one_venture_x_rewards(current_transactions), "capital_one_venture_x_rewards"))
-        res.append((capital_one_spark_miles_for_business(current_transactions), "capital_one_spark_miles_for_business"))
-        res.append((chase_sapphire_preferred(current_transactions), "chase_sapphire_preferred"))
-        res.append((chase_sapphire_reserve(current_transactions), "chase_sapphire_reserve"))
-        res.append((chase_ink_business_unlimited(current_transactions), "chase_ink_business_unlimited"))
-        res.append((citi_custom_cash(current_transactions), "citi_custom_cash"))
-        res.append((citi_double_cash(current_transactions), "citi_double_cash"))
-        res.append((citi_premier(current_transactions), "citi_premier"))
-        res.append((citi_rewards(current_transactions), "citi_rewards"))
-        res.append((citi_aadvantage_platinum_select(current_transactions), "citi_aadvantage_platinum_select"))
-        res.append((discover_it_cash_back(current_transactions), "discover_it_cash_back"))
-        res.append((discover_it_balance_transfer(current_transactions), "discover_it_balance_transfer"))
-        res.append((discover_student_credit_card(current_transactions), "discover_student_credit_card"))
-        res.append((discover_it_miles(current_transactions), "discover_it_miles"))
-        res.append((discover_it_chrome(current_transactions), "discover_it_chrome"))
-        res.append((synchrony_sams_club_mastercard(current_transactions), "synchrony_sams_club_mastercard"))
-        res.append((us_bank_cash_plus_visa_signature(current_transactions), "us_bank_cash_plus_visa_signature"))
-        res.append((us_bank_business_cash_rewards_world_elite_mastercard(current_transactions), "us_bank_business_cash_rewards_world_elite_mastercard"))
-        res.append((wells_fargo_active_cash(current_transactions), "wells_fargo_active_cash"))
-        res.append((discover_it_balance_transfer(current_transactions), "discover_it_balance_transfer"))
-        res.append((discover_student_credit_card(current_transactions), "discover_student_credit_card"))
-        res.append((wells_fargo_autograph(current_transactions), "wells_fargo_autograph"))
-        res.append((usaa_cashback_rewards_plus_american_express(current_transactions), "usaa_cashback_rewards_plus_american_express"))
-        res.append((usaa_preferred_cash_rewards_visa_signature(current_transactions), "usaa_preferred_cash_rewards_visa_signature"))
-        res.append((usaa_rewards_american_express(current_transactions), "usaa_rewards_american_express"))
+        res.append((card_backtesting.amex_platinum(current_transactions), "amex_platinum"))
+        res.append((card_backtesting.amex_gold(current_transactions), "amex_gold"))
+        res.append((card_backtesting.amex_blue_cash_everyday(current_transactions), "amex_blue_cash_everyday"))
+        res.append((card_backtesting.amex_blue_cash_preferred(current_transactions), "amex_blue_cash_preferred"))
+        res.append((card_backtesting.amex_business_gold(current_transactions), "amex_business_gold"))
+        res.append((card_backtesting.bofa_travel_rewards(current_transactions), "bofa_travel_rewards"))
+        res.append((card_backtesting.bofa_premium_rewards(current_transactions), "bofa_premium_rewards"))
+        res.append((card_backtesting.bofa_unlimited_rewards(current_transactions), "bofa_unlimited_rewards"))
+        res.append((card_backtesting.barclays_aviator_red_world_elite_mastercard(current_transactions), "barclays_aviator_red_world_elite_mastercard"))
+        res.append((card_backtesting.barclays_the_hawaiian_airlines_world_elite_mastercard(current_transactions), "barclays_the_hawaiian_airlines_world_elite_mastercard"))
+        res.append((card_backtesting.barclays_jetblue(current_transactions), "barclays_jetblue"))
+        res.append((card_backtesting.capital_one_quicksilver_cash_rewards(current_transactions), "capital_one_quicksilver_cash_rewards"))
+        res.append((card_backtesting.capital_one_venture_rewards(current_transactions), "capital_one_venture_rewards"))
+        res.append((card_backtesting.capital_one_venture_x_rewards(current_transactions), "capital_one_venture_x_rewards"))
+        res.append((card_backtesting.capital_one_spark_miles_for_business(current_transactions), "capital_one_spark_miles_for_business"))
+        res.append((card_backtesting.chase_sapphire_preferred(current_transactions), "chase_sapphire_preferred"))
+        res.append((card_backtesting.chase_sapphire_reserve(current_transactions), "chase_sapphire_reserve"))
+        res.append((card_backtesting.chase_ink_business_unlimited(current_transactions), "chase_ink_business_unlimited"))
+        res.append((card_backtesting.citi_custom_cash(current_transactions), "citi_custom_cash"))
+        res.append((card_backtesting.citi_double_cash(current_transactions), "citi_double_cash"))
+        res.append((card_backtesting.citi_premier(current_transactions), "citi_premier"))
+        res.append((card_backtesting.citi_rewards(current_transactions), "citi_rewards"))
+        res.append((card_backtesting.citi_aadvantage_platinum_select(current_transactions), "citi_aadvantage_platinum_select"))
+        res.append((card_backtesting.discover_it_cash_back(current_transactions), "discover_it_cash_back"))
+        res.append((card_backtesting.discover_it_balance_transfer(current_transactions), "discover_it_balance_transfer"))
+        res.append((card_backtesting.discover_student_credit_card(current_transactions), "discover_student_credit_card"))
+        res.append((card_backtesting.discover_it_miles(current_transactions), "discover_it_miles"))
+        res.append((card_backtesting.discover_it_chrome(current_transactions), "discover_it_chrome"))
+        res.append((card_backtesting.synchrony_sams_club_mastercard(current_transactions), "synchrony_sams_club_mastercard"))
+        res.append((card_backtesting.us_bank_cash_plus_visa_signature(current_transactions), "us_bank_cash_plus_visa_signature"))
+        res.append((card_backtesting.us_bank_business_cash_rewards_world_elite_mastercard(current_transactions), "us_bank_business_cash_rewards_world_elite_mastercard"))
+        res.append((card_backtesting.wells_fargo_active_cash(current_transactions), "wells_fargo_active_cash"))
+        res.append((card_backtesting.discover_it_balance_transfer(current_transactions), "discover_it_balance_transfer"))
+        res.append((card_backtesting.discover_student_credit_card(current_transactions), "discover_student_credit_card"))
+        res.append((card_backtesting.wells_fargo_autograph(current_transactions), "wells_fargo_autograph"))
+        res.append((card_backtesting.usaa_cashback_rewards_plus_american_express(current_transactions), "usaa_cashback_rewards_plus_american_express"))
+        res.append((card_backtesting.usaa_preferred_cash_rewards_visa_signature(current_transactions), "usaa_preferred_cash_rewards_visa_signature"))
+        res.append((card_backtesting.usaa_rewards_american_express(current_transactions), "usaa_rewards_american_express"))
 
-        res.sort(lambda x: x[0], reverse=True)
+        res.sort(key = lambda x: x[0], reverse=True)
 
-        return [item[1] for item in res[:5]]
+        return [item for item in res[:5]]
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
--- packages/backend/transactions.py
+++ packages/backend/transactions.py
@@ -1,11 +1,13 @@
 import heapq
+
+
 class Transactions:
 
-    def __init__(self, response):
-        self.flights = 0.0 # 'Airlines and Aviation Services'
+    def __init__(self):
+        self.flights = 0.0  # 'Airlines and Aviation Services'
         self.groceries = 0.0
-        self.gas = 0.0 # 'Taxi'
-        self.dining = 0.0 # 'Restaurants'
+        self.gas = 0.0  # 'Taxi'
+        self.dining = 0.0  # 'Restaurants'
         self.entertainment = 0.0
         self.car_rental = 0.0
         self.hotel = 0.0
@@ -14,64 +16,65 @@ class Transactions:
         self.digital_wallet = False
         self.total = 0.0
 
-        for transaction in response:
-            if transaction.category == "Flights":
-                self.flights += transaction.amount
-            elif transaction.category == "Groceries":
-                self.groceries += transaction.amount
-            elif transaction.category == "Gas":
-                self.gas += transaction.amount
-            elif transaction.category == "Dining":
-                self.dining += transaction.amount
-            elif transaction.category == "Entertainment":
-                self.entertainment += transaction.amount
-            elif transaction.category == "Car Rental":
-                self.car_rental += transaction.amount
-            elif transaction.category == "Hotel":
-                self.hotel += transaction.amount
-
-            self.total += transaction.amount
+    #         for transaction in response:
+    #             if transaction.category == "Flights":
+    #                 self.flights += transaction.amount
+    #             elif transaction.category == "Groceries":
+    #                 self.groceries += transaction.amount
+    #             elif transaction.category == "Gas":
+    #                 self.gas += transaction.amount
+    #             elif transaction.category == "Dining":
+    #                 self.dining += transaction.amount
+    #             elif transaction.category == "Entertainment":
+    #                 self.entertainment += transaction.amount
+    #             elif transaction.category == "Car Rental":
+    #                 self.car_rental += transaction.amount
+    #             elif transaction.category == "Hotel":
+    #                 self.hotel += transaction.amount
+    #
+    #             self.total += transaction.amount
 
     def set_benefits(self, benefits):
         self.benefits = benefits
 
-def extract_max_values(k):
-    # Create a max heap (negate the values to simulate a max heap)
+    def extract_max_values(self, k):
+        # Create a max heap (negate the values to simulate a max heap)
+        max_heap = []
 
-    heapq.heappush(max_heap, (-self.card.flights, "Flights"))
-    heapq.heappush(max_heap, (-self.card.groceries, "Groceries"))
-    heapq.heappush(max_heap, (-self.card.gas, "Gas"))
-    heapq.heappush(max_heap, (-self.card.dining, "Dining"))
-    heapq.heappush(max_heap, (-self.card.entertainment, "Entertainment"))
-    heapq.heappush(max_heap, (-self.card.car_rental, "Car Rental"))
-    heapq.heappush(max_heap, (-self.card.hotel, "Hotel"))
-    heapq.heappush(max_heap, (-self.card.retail, "Retail"))
+        heapq.heappush(max_heap, (-self.flights, "Flights"))
+        heapq.heappush(max_heap, (-self.groceries, "Groceries"))
+        heapq.heappush(max_heap, (-self.gas, "Gas"))
+        heapq.heappush(max_heap, (-self.dining, "Dining"))
+        heapq.heappush(max_heap, (-self.entertainment, "Entertainment"))
+        heapq.heappush(max_heap, (-self.car_rental, "Car Rental"))
+        heapq.heappush(max_heap, (-self.hotel, "Hotel"))
+        heapq.heappush(max_heap, (-self.retail, "Retail"))
 
+        max_values = []
+        for i in range(k):
+            if max_heap:
+                _, category = heapq.heappop(max_heap)  # Negate back to get the actual max value
+                max_values.append(category)
+            else:
+                break
 
-    max_values = []
-    for _ in range(k):
-        if max_heap:
-            max_value = -heapq.heappop(max_heap)  # Negate back to get the actual max value
-            max_values.append(max_value)
-        else:
-            break
+        return max_values
 
-    return max_values
-
-def add_transaction(self, category, amount):
-    if category == "Airlines and Aviation Services":
-        self.flights += amount
-    elif category == "Groceries":
-        self.groceries += amount
-    elif category == "Gas":
-        self.gas += amount
-    elif category == "Restaurants":
-        self.dining += amount
-    elif category == "Entertainment":
-        self.entertainment += amount
-    elif category == "Car Rental":
-        self.car_rental += amount
-    elif category == "Hotel":
-        self.hotel += amount
-    elif category == "Retail":
-        self.retail += amount
+    def add_transaction(self, category, amount):
+        self.total += amount
+        if category == "Airlines and Aviation Services":
+            self.flights += amount
+        elif category == "Groceries":
+            self.groceries += amount
+        elif category == "Gas":
+            self.gas += amount
+        elif category == "Restaurants":
+            self.dining += amount
+        elif category == "Entertainment":
+            self.entertainment += amount
+        elif category == "Car Rental":
+            self.car_rental += amount
+        elif category == "Hotel":
+            self.hotel += amount
+        elif category == "Retail":
+            self.retail += amount
