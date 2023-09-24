96c348f9d595ffec33e20760a6d9c4db39f7bc90
brandon-nguyen-lam <brandonnguyenlam@gmail.com>
2023-09-24 02:32:58 -0500

    💀 PLEASEEEEEEEEEEEEEEEEEE

--- packages/backend/server.py
+++ packages/backend/server.py
@@ -4,153 +4,31 @@ import os
 import datetime as dt
 import json
 import time
-
-import plaid
-from plaid.api import plaid_api
-from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
-from plaid.model.link_token_create_request import LinkTokenCreateRequest
-from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
-from plaid.model.products import Products
-from plaid.model.country_code import CountryCode
-from plaid.model.auth_get_request import AuthGetRequest
-from plaid.model.transactions_sync_request import TransactionsSyncRequest
+# from flask_cors import CORS
 
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import jsonify
+from flask_cors import CORS, cross_origin
+import requests
 
 load_dotenv()
-app = Flask(name)
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
-PLAID_SECRET = os.getenv('PLAID_SECRET')
-PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
-PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions').split(',')
-PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US').split(',')
+@app.route('/')
+@cross_origin()
+def hello_world():
+    return 'Hello World!'
 
-configuration = plaid.Configuration(
-  host=plaid.Environment.Sandbox,
-  api_key={
-    'clientId': PLAID_CLIENT_ID,
-    'secret': PLAID_SECRET,
-    'Plaid-Version': '2020-09-14',
-  }
-)
-
-api_client = plaid.ApiClient(configuration)
-client = plaid_api.PlaidApi(api_client)
-
-products = []
-for product in PLAID_PRODUCTS:
-    products.append(Products(product))
-
-access_token = None
-
-@app.route('/api/info', methods=['POST'])
-def info():
-    global access_token
-    global item_id
-    return jsonify({
-        'item_id': item_id,
-        'access_token': access_token,
-        'products': PLAID_PRODUCTS
-    })
-
-@app.route('/api/create_link_token', methods=['POST'])
-def create_link_token():
-    try:
-        request = LinkTokenCreateRequest(
-            products=products,
-            client_name="Plaid Test App",
-            country_codes=list(map(lambda x: CountryCode(x), PLAID_COUNTRY_CODES)),
-            language='en',
-            user=LinkTokenCreateRequestUser(
-                client_user_id=str(time.time())
-            )
-        )
-        if PLAID_REDIRECT_URI!=None:
-            request['redirect_uri']=PLAID_REDIRECT_URI
-    # create link token
-        response = client.link_token_create(request)
-        return jsonify(response.to_dict())
-    except plaid.ApiException as e:
-        return json.loads(e.body)
-
-@app.route('/api/set_access_token', methods=['POST'])
-def get_access_token():
-    global access_token
-    global item_id
-    global transfer_id
-    public_token = request.form['public_token']
-    try:
-        exchange_request = ItemPublicTokenExchangeRequest(
-            public_token=public_token)
-        exchange_response = client.item_public_token_exchange(exchange_request)
-        access_token = exchange_response['access_token']
-        item_id = exchange_response['item_id']
-        if 'transfer' in PLAID_PRODUCTS:
-            transfer_id = authorize_and_create_transfer(access_token)
-        return jsonify(exchange_response.to_dict())
-    except plaid.ApiException as e:
-        return json.loads(e.body)
-
-@app.route('/api/auth', methods=['GET'])
-def get_auth():
-    try:
-       request = AuthGetRequest(
-            access_token=access_token
-        )
-       response = client.auth_get(request)
-       pretty_print_response(response.to_dict())
-       return jsonify(response.to_dict())
-    except plaid.ApiException as e:
-        error_response = format_error(e)
-        return jsonify(error_response)
-
-@app.route('/api/transactions', methods=['GET'])
+@app.route('/api/transactions', methods=['GET', 'POST'])
+@cross_origin()
 def get_transactions():
-    # Set cursor to empty to receive all historical updates
-    cursor = ''
+    req = request.json
+    return jsonify(req)
 
-    # New transaction updates since "cursor"
-    added = []
-    modified = []
-    removed = [] # Removed transaction ids
-    has_more = True
-    try:
-        # Iterate through each page of new transaction updates for item
-        while has_more:
-            request = TransactionsSyncRequest(
-                access_token=access_token,
-                cursor=cursor,
-            )
-            response = client.transactions_sync(request).to_dict()
-            # Add this page of results
-            added.extend(response['added'])
-            modified.extend(response['modified'])
-            removed.extend(response['removed'])
-            has_more = response['has_more']
-            # Update cursor to the next cursor
-            cursor = response['next_cursor']
-            pretty_print_response(response)
+if __name__ == '__main__':
+     app.run(debug=True, port=int(os.getenv('PORT', 8000)))
 
-        # Return the 8 most recent transactions
-        latest_transactions = sorted(added, key=lambda t: t['date'])[-8:]
-        return jsonify({
-            'latest_transactions': latest_transactions})
-
-    except plaid.ApiException as e:
-        error_response = format_error(e)
-        return jsonify(error_response)
-
-def pretty_print_response(response):
-  print(json.dumps(response, indent=2, sort_keys=True, default=str))
-
-def format_error(e):
-    response = json.loads(e.body)
-    return {'error': {'status_code': e.status, 'display_message':
-                      response['error_message'], 'error_code': response['error_code'], 'error_type': response['error_type']}}
-
-if __name__ == "__main__":
-     app.run(port=int(os.getenv('PORT', 8000)))
